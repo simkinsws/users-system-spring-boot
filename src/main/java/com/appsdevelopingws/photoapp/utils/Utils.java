062a0c79ea5727aa6777ecf6821176682f02f226
unknown <nir.bilchinski@gmail.com>
2022-01-23 17:14:19 +0200

    added user-dto , user-service, user-controller and userId Generator.

--- src/main/java/com/appsdevelopingws/photoapp/utils/Utils.java
+++ src/main/java/com/appsdevelopingws/photoapp/utils/Utils.java
@@ -8,6 +8,7 @@ import java.util.Random;
 @Component
 public class Utils {
     private final Random RANDOM = new SecureRandom();
+
     private final String ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     private final int ITERATIONS = 10000;
     private final int KEY_LENGTH = 256;
